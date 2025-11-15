af13026cda3892bd45cf7a91bf3c3a1cc9a113fb
Hinks <henrik.nr10@gmail.com>
2025-11-15 10:57:43 +0100

    start on navigation

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser exposing (UrlRequest)
 import Element
+import Element.Input as Input
 import Url exposing (Url)
 
 
@@ -11,6 +12,7 @@ type alias Model =
 
 type Msg
     = NoOp
+    | NavigateTo String
 
 
 init : Model
@@ -20,15 +22,65 @@ init =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "My App"
-    , body = [ Element.layout [] (Element.text model.value) ]
+    { title = "PokerNight App"
+    , body =
+        [ Element.layout
+            []
+            (Element.column
+                [ Element.width Element.fill
+                , Element.spacing 0
+                ]
+                [ -- Title row
+                  Element.row
+                    [ Element.width Element.fill
+                    , Element.padding 20
+                    , Element.spacing 0
+                    ]
+                    [ Element.text "PokerNight App" ]
+
+                -- Navigation row
+                , Element.row
+                    [ Element.width Element.fill
+                    , Element.padding 15
+                    , Element.spacing 15
+                    ]
+                    [ navButton "Home"
+                    , navButton "Players"
+                    , navButton "Game"
+                    , navButton "Champion"
+                    ]
+
+                -- Content area
+                , Element.el
+                    [ Element.width Element.fill
+                    , Element.padding 20
+                    ]
+                    (Element.text model.value)
+                ]
+            )
+        ]
     }
+
+
+navButton : String -> Element.Element Msg
+navButton label =
+    Input.button
+        [ Element.padding 10
+        , Element.spacing 5
+        ]
+        { onPress = Just (NavigateTo label)
+        , label = Element.text ("[ " ++ label ++ " ]")
+        }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
+            ( model, Cmd.none )
+
+        NavigateTo route ->
+            -- Placeholder for navigation - can be wired to routing later
             ( model, Cmd.none )
 
 
